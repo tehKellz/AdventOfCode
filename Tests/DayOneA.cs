c7955dfb0851614cb382b5c4ac4a4cd67446664e
tehKellz <kellywashington@gmail.com>
2020-12-01 00:48:20 +0000

    Auto run challenges.

---
+++ Tests/DayOneA.cs
@@ -0,0 +1,18 @@
+using System;
+using AdventOfCode;
+
+namespace AdventOfCode
+{
+  class DayOneA : CodeTest
+  {
+    public DayOneA() {}
+
+    public bool Enabled => false;
+
+    public string Run()
+    {
+      Console.WriteLine("Hello World Day One");
+      return "output";
+    }
+  }
+}
---
+++ main.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AdventOfCode
+{
+  public interface CodeTest
+  {
+    bool Enabled { get; }
+    string Run();
+  }
+
+  public class AdventOfCodeMain
+  {
+    private static List<CodeTest> Tests = new List<CodeTest>();
+
+    AdventOfCodeMain() {}
+    static void Main(string[] args)
+    {
+      Assembly a = Assembly.LoadFrom("aoc_test.exe");
+      // Gets the type names from the assembly.
+      Type[] types2 = a.GetTypes();
+      foreach (Type t in types2)
+      {
+          if (t.GetInterface("AdventOfCode.CodeTest") != null)
+          {
+            Console.WriteLine("Found test " + t.FullName);
+            CodeTest instance = (CodeTest)Activator.CreateInstance(t);
+            Tests.Add(instance);
+          }
+      }
+
+      Console.WriteLine("Running Tests.");
+      foreach(var test in Tests)
+      {
+        string output = "<disabled>";
+        if (test.Enabled)
+        {
+          output = test.Run();
+        }
+          
+        Console.WriteLine("  [" + test.GetType().ToString() + "]: " + output);
+      }
+      Console.WriteLine("Run complete.");
+    }
+  }
+}
