3daa887e9c5e1fae4617f9f37ed0889ca8df5cdc
tehKellz <kellywashington@gmail.com>
2020-12-09 06:03:39 +0000

    Condensed Day 9

--- 2020/Day9.cs
+++ 2020/Day9.cs
@@ -8,72 +8,37 @@ class Day9 : CodeTest
 {
     public bool Enabled => true;
 
+    Int64 weakness = -1;
     List<Int64> Seq = new List<Int64>();
+    public void Init() { Utils.Load("2020/Day9.input", Seq); }
 
-    public void Init() 
+    public bool Valid(int start, int end, Int64 value)
     {
-        Utils.Load("2020/Day9.input", Seq);
-    }
-
-    public bool Valid(List<Int64> range, Int64 value)
-    {
-
-        for(int i = 0; i<range.Count;++i)
-        {
-            for(int j=i+1; j<range.Count;++j)
-            {
-                if (range[i] + range[j] == value)
-                {
-                    return true;
-                } 
-            }
-        }
+        for(int i=start; i<end;++i)
+            for(int j=i+1; j<end;++j)
+                if (Seq[i] + Seq[j] == value) return true;
         return false;
     }
 
     public string RunA()
     {
         int curr = 25;
-
-        while(Valid(Seq.GetRange(curr - 25, 25), Seq[curr]))
-        {
-            ++curr;
-        }
-//        Console.WriteLine($"C:[{curr}] {Seq[curr]} => {string.Join(",", Seq.GetRange(curr - 25, 25).Select(x => x.ToString()).ToArray())}");
-        
-
-        return Seq[curr].ToString();
-    }
-
-    Int64 FindMinMaxSum(List<Int64> range)
-    {
-        Int64 min = 375054920;
-        Int64 max = 0;
-        foreach(Int64 v in range)
-        {
-            if (v < min) min = v;
-            if (v > max) max = v;
-        }
-        return min + max;
+        while(Valid(curr-25, curr, Seq[curr])) ++curr;
+        return (weakness = Seq[curr]).ToString();
     }
 
     public string RunB()
     {
-        Int64 target = 375054920;
         for(int i=0;i<Seq.Count;++i)
         {
             Int64 sum = 0;
-            for(int j=i;j<Seq.Count;++j)
+            for(int j=i;sum<weakness;++j)
             {
                 sum += Seq[j];
-                if (sum == target)
-                {
-                    return $"[{i}] {Seq[i]} + [{j}] {Seq[j]} = {FindMinMaxSum(Seq.GetRange(i, j-i))}";
-                }
-                else if (sum > target) break;
+                if (sum == weakness) return (Seq.GetRange(i, j-i).Min() + Seq.GetRange(i, j-i).Max()).ToString();
             }
         }
-        return "Template Output B";
+        return "Unbreakable code";
     }
 }
 }
